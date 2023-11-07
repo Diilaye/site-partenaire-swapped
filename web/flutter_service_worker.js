c4fec85c628f4dc39f98fa483e061d8d31207e5d
Issa laye KANE <issa.k@deally.fr>
2023-11-07 16:29:39 +0000

    add new version

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "44597eab59a9a31e6ac3f391ee39de6e",
-"index.html": "f9561cda5ed02ce898ae8e05cf72dcb1",
-"/": "f9561cda5ed02ce898ae8e05cf72dcb1",
-"main.dart.js": "ecef36a170011b8e236084ec024e105b",
+"index.html": "eb1efe456340d5cfdcc2be6a30468843",
+"/": "eb1efe456340d5cfdcc2be6a30468843",
+"main.dart.js": "e0b91accdd6867adbfb1359e624e54a6",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon1.png": "5dcef449791fa27946b3d35ad8803796",
 "favicon.png": "bb480d52b46839c00e20f2ba1725eb1c",
