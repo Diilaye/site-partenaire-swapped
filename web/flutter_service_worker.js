825951c529d358a99a994a1f9d52f62db08616c7
Issa laye KANE <issa.k@deally.fr>
2024-01-15 19:39:00 +0000

    add news version

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "44597eab59a9a31e6ac3f391ee39de6e",
-"index.html": "798d75aead79b8b2d6572e86e9b62566",
-"/": "798d75aead79b8b2d6572e86e9b62566",
-"main.dart.js": "91967bcbebb39ab7ae354f322b210692",
+"index.html": "55c65ddbf3de2c24848d29836d0b2348",
+"/": "55c65ddbf3de2c24848d29836d0b2348",
+"main.dart.js": "ad01ddb4f088bc2775a817bb8cf617b8",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon1.png": "5dcef449791fa27946b3d35ad8803796",
 "favicon.png": "bb480d52b46839c00e20f2ba1725eb1c",
