f8803e2a6c089d3d4b350646833c611da1468e99
Issa laye KANE <issa.k@deally.fr>
2024-05-06 21:44:56 +0000

    add zonedecaissement

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "44597eab59a9a31e6ac3f391ee39de6e",
-"index.html": "d0e231ebbcb330c2c9dc29b9a4baab42",
-"/": "d0e231ebbcb330c2c9dc29b9a4baab42",
-"main.dart.js": "643a24b48ae3e3d14a3f327c15088af7",
+"index.html": "017351b48b6616f2f0c5db00216b923c",
+"/": "017351b48b6616f2f0c5db00216b923c",
+"main.dart.js": "0a92b4f166b55545ec764405ec393461",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon1.png": "5dcef449791fa27946b3d35ad8803796",
 "favicon.png": "bb480d52b46839c00e20f2ba1725eb1c",
